c7c15ea7a33601802cc73f1bfacba1211acac47b
Mario Zhu <kaido89@users.noreply.github.com>
2017-09-08 10:23:27 +0100

    Input Types

---
+++ Scripts/025-Script-08-09-2017.php
@@ -0,0 +1,13 @@
+<html>
+  <body>
+    This game is:
+    <?php echo $_POST["game"]; ?>
+    <br>This description is:
+    <?php echo $_POST["description"]; ?>
+    <br>The Gender is:
+    <?php echo $_POST["gender"]; ?>
+    <br>You like:
+    <?php echo $_POST["like1"]; ?>
+    <?php echo $_POST["like2"]; ?>
+  </body>
+</html>
