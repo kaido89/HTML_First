4ddb553c78bab8f99e50fe033ab4249109e602b1
Mario Zhu <kaido89@users.noreply.github.com>
2017-09-09 10:38:52 +0100

    Input Attributes

---
+++ Scripts/026-Script-09-09-2017.php
@@ -0,0 +1,15 @@
+<html>
+  <body>
+    This game is:
+    <?php echo $_POST["game"]; ?>
+    <br>This description is:
+    <?php echo $_POST["description"]; ?>
+    <br>This Year is:
+    <?php echo $_POST["year"]; ?>
+    <br>This Month is:
+    <?php echo $_POST["month"]; ?>
+    <br>This score is:
+    <?php echo $_POST["score"]; ?>
+    <br>This like is:
+    <?php echo $_POST["like"]; ?>
+</html>
